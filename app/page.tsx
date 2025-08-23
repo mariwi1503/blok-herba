708b6f05797b78f64412a355b21ef9537842c034
ary <mariwi1503@gmail.com>
2025-08-23 12:33:34 +0700

    remove visi and misi component from hero section

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function HomePage() {
       <Navbar />
       <main>
         <HeroSection />
-        <VisionMissionSection />
+        {/* <VisionMissionSection /> */}
         <StatsSection />
         <FinanceSection />
         <ActivitiesSection />
